6e555bcf92ebb70a0f2684feb5afb0b3794dfbf1
GunjanRawat21105031 <97359837+GunjanRawat21105031@users.noreply.github.com>
2022-01-21 17:19:51 +0530

    Create Assignment2_Outputs.py

---
+++ Assignment2_Outputs.py
@@ -0,0 +1,49 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question1.py =
+lenght of string= 35
+Reversed_string= egaugnal evitisnes esac a si nohtyp
+new_string=  a case sensitive
+new string= python is a object oriented language
+10
+pythonisacasesensitivelanguage
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question2.py =
+Enter your name:Gunjan Rawat
+Enter student ID:21105031
+Enter department name:ECE
+Enter CGPA:9.9
+Hey, Gunjan Rawat Here!
+My SID is 21105031
+I am from ECE department and my CGPA is 9.9
+
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question3.py =
+a & b= 8
+a | b = 58
+a ^ b = 50
+2 bit left shift of a= 224
+2 bit left shift of a= 40
+2 bit right shift of a= 14
+4 bit left shift of b= 0
+
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question4.py =
+Enter 1st no. : 43
+Enter 2nd no. : 67
+Enter 3rd no. : 103
+Highest no. = 103
+
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question5.py =
+Enter any wordtime
+No
+
+
+= RESTART: C:\Users\GUNJAN\Desktop\PEC\Classes\Assignments\Python\Question6.py =
+Lenght of first side=13
+Lenght of second side=12
+Lenght of third side=5
+Is triangle possible?
+Yes
